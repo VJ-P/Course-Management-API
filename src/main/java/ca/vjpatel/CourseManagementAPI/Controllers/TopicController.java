118d7861152cb149f674391478f0951eb8ced006
VJ-P <108.vijayp@gmail.com>
2020-11-08 01:23:42 -0500

    Update API for POST mapping

--- src/main/java/ca/vjpatel/CourseManagementAPI/Controllers/TopicController.java
+++ src/main/java/ca/vjpatel/CourseManagementAPI/Controllers/TopicController.java
@@ -3,9 +3,7 @@ package ca.vjpatel.CourseManagementAPI.Controllers;
 import ca.vjpatel.CourseManagementAPI.Models.Topic;
 import ca.vjpatel.CourseManagementAPI.Services.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,13 +14,19 @@ public class TopicController {
     @Autowired
     private TopicService topicService;
 
-    @RequestMapping("/topics")
+    @RequestMapping(method = RequestMethod.GET, value = "/topics")
     public List<Topic> getAllTopics() {
         return topicService.getAllTopics();
     }
 
-    @RequestMapping("/topics/{id}")
+    @RequestMapping(method = RequestMethod.GET, value = "/topics/{id}")
     public Topic getTopic(@PathVariable String id) {
         return topicService.getTopic(id);
     }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
+    }
+
 }
